06a7ace50ba29547498dbe0d8b98bc12b78d0e84
Vivek Kumar <viveksingh0143@gmail.com>
2018-07-23 16:11:34 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,128 @@
+import axios from "axios";
+/**
+ * Create a new Axios client instance
+  */
+
+const { API_BASE } = process.env.API_BASE_URL;
+const getClient = (baseUrl = null) => {
+  if (baseUrl === null) {
+    baseUrl = API_BASE;
+  }
+  const options = {
+    baseURL: baseUrl
+  };
+
+  const client = axios.create(options);
+  return client;
+};
+
+class ApiClient {
+  constructor(baseUrl = null) {
+    this.client = getClient(baseUrl);
+  }
+
+  get(url, conf = {}) {
+    return this.client
+      .get(url, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  }
+
+  delete(url, conf = {}) {
+    return this.client
+      .delete(url, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  }
+
+  head(url, conf = {}) {
+    return this.client
+      .head(url, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  }
+
+  options(url, conf = {}) {
+    return this.client
+      .options(url, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  }
+
+  post(url, data = {}, conf = {}) {
+    return this.client
+      .post(url, data, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  }
+
+  put(url, data = {}, conf = {}) {
+    return this.client
+      .put(url, data, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  }
+
+  patch(url, data = {}, conf = {}) {
+    return this.client
+      .patch(url, data, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  }
+}
+
+export { ApiClient };
+
+/**
+ * Base HTTP Client
+ */
+export default {
+  get(url, conf = {}) {
+    return getClient()
+      .get(url, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  },
+
+  delete(url, conf = {}) {
+    return getClient()
+      .delete(url, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  },
+
+  head(url, conf = {}) {
+    return getClient()
+      .head(url, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  },
+
+  options(url, conf = {}) {
+    return getClient()
+      .options(url, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  },
+
+  post(url, data = {}, conf = {}) {
+    return getClient()
+      .post(url, data, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  },
+
+  put(url, data = {}, conf = {}) {
+    return getClient()
+      .put(url, data, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  },
+
+  patch(url, data = {}, conf = {}) {
+    return getClient()
+      .patch(url, data, conf)
+      .then(response => Promise.resolve(response))
+      .catch(error => Promise.reject(error));
+  }
+};
